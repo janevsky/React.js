79a7cc6bea79a8ecf0335c9f162422461d94b0b8
janevsky <baze.janevski@gmail.com>
2021-02-22 22:04:54 +0100

    onclick func added 2nd button

--- cas5/src/components/App.js
+++ cas5/src/components/App.js
@@ -5,6 +5,7 @@ const App = () => {
 
 //let c = 0;
 const [counter, setCounter] = useState(0);
+const [username, setUsername] = useState('');
 
 const onButtonClick = () => {
   // c+= 5;
@@ -18,14 +19,27 @@ const onButtonClick = () => {
   //setCouner(counter + 1); !!!! <--- nikako, radi al se ne cue!
   console.log('on button clicked', counter);
 };
+
+const onButtonClick1 = () => {
+  console.log(username);
+}
+
+const onChangeUsername = (event) => {
+  setUsername(event.target.value)
+};
+
 return (
   console.log('app'),
   <div className='app'>
       {/*<p>Counter: {c}</p>*/}
       <p>Counter: {counter}</p>
       <button onClick={onButtonClick}>Click me</button>
+      <br/>
+      <br/>
+      <input type='text' placeholder='username' value={username} onChange={onChangeUsername}/>
+      <button onClick={onButtonClick1}>Click me</button>
   </div>
-)
+);
 
 };
 export default App;
