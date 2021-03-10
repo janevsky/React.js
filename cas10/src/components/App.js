5c24e96d37aa19ce3f0687671e07cc1c35f85e70
janevsky <baze.janevski@gmail.com>
2021-03-10 21:39:30 +0100

    first commit

--- cas10/src/components/App.js
+++ cas10/src/components/App.js
@@ -1,10 +1,29 @@
-
+import { useEffect, useState } from "react";
 const App = () => {
+  const [albums, setAlbums] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/albums")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setAlbums(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
   return (
     <div className="App">
-
+      <div className="albums">
+        {albums.map((album) => {
+          return <p key={album.id}>{album.title}</p>;
+        })}
+      </div>
     </div>
   );
-}
-
+};
 export default App;
