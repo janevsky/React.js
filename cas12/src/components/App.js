d9794cb2a3ffa073775c8aaa13452ae62f4380b4
janevsky <baze.janevski@gmail.com>
2021-03-17 21:36:06 +0100

    fetch users

--- cas12/src/components/App.js
+++ cas12/src/components/App.js
@@ -1,5 +1,9 @@
 // vendor imports 
+import { useState, useEffect } from 'react';
 import { Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+// actions
+import { setUsers } from '../redux/ducks/users';
 // components
 import Home from '../components/pages/Home';
 import Users from '../components/pages/Users';
@@ -8,15 +12,54 @@ import NavMenu from '../components/widgets/navmenu/NavMenu';
 // styles
 import '../assets/styles/global.css';
 
-const App = () => {
+const App = (props) => {
+  const [isDataFetched, setIsDataFetched] = useState(false);
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(data => {
+        setIsDataFetched(true);
+        props.setUsers(data);
+      })
+      .catch(error => {
+        setIsDataFetched(true);
+        setError('Error fetching users');
+      });
+      // eslint-disable-next-line
+  }, []);
+
   return (
-    <>
-      <NavMenu />
-      <Route exact path='/' component={Home} />
-      <Route exact path='/users' component={Users} />
-      <Route exact path='/stats' component={Stats} />
-    </>
+    console.log('app props', props),
+    isDataFetched
+      ?
+      error === ''
+        ?
+        <>
+          <NavMenu />
+          <Route exact path='/' component={Home} />
+          <Route exact path='/users' component={Users} />
+          <Route exact path='/stats' component={Stats} />
+        </>
+        :
+        <h2>{error}</h2>
+      :
+      <h2>Fetching data...</h2>
   );
 };
 
-export default App;
+const mapStateToProps = state => {
+  console.log('state', state);
+  return {
+    users: state.users
+  }
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    setUsers: (users) => { dispatch(setUsers(users)) }
+  }
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- cas12/src/components/pages/Users.js
+++ cas12/src/components/pages/Users.js
@@ -1,4 +1,7 @@
-const Users = () => {
+// vendor imports
+import { connect } from 'react-redux';
+
+const Users = (props) => {
     // const [users, setUsers] = useState([]);
     // setUsers(data);
     /*  setCounter = (state => {
@@ -7,12 +10,25 @@ const Users = () => {
     // car.model = 'Audi'; !!!
 
     return (
+        console.log('users props', props),
         <div className='wrapper'>
             <div className='users'>
                 <h2>This is Users</h2><br />
+                {props.users.map(user => {
+                    return (
+                        <p key={user.id}>{user.name}</p>
+                    )
+                })
+                }
             </div>
         </div >
     );
 };
 
-export default Users;
+const mapStateToProps = state => {
+    return {
+        users: state.users
+    }
+};
+
+export default connect(mapStateToProps)(Users);
